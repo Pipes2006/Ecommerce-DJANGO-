8b173fcabbafade51422bf5a1f27311edc3cea4c
Pipes2006 <andrelip2006@gmail.com>
2025-09-05 11:17:48 -0500

    Retoques en la base de datos MER

--- tienda/admin.py
+++ tienda/admin.py
@@ -7,12 +7,21 @@ from .models import Pago
 admin.site.register(Cliente)
 admin.site.register(CategoriaProducto)
 admin.site.register(Producto)
+from django.utils.html import format_html
+
 @admin.register(Pago)
 class PagoAdmin(admin.ModelAdmin):
-	list_display = ("nombre", "telefono", "total", "estado", "fecha")
+	list_display = ("nombre", "telefono", "total", "estado", "estado_icono", "fecha")
 	list_filter = ("estado", "fecha")
 	search_fields = ("nombre", "telefono")
 	list_editable = ("estado",)
+
+	def estado_icono(self, obj):
+		if obj.estado == "confirmado":
+			return format_html('<span style="color:green;font-size:1.3em;">✔️</span>')
+		else:
+			return format_html('<span style="color:#d32f2f;font-size:1.3em;">❌</span>')
+	estado_icono.short_description = "Confirmación"
 admin.site.register(Pedido)
 admin.site.register(DetallePedido)
 
